56c7d8406cccc052b0b2eaa3208b34ded54f5f64
Long Hoang <87174590+longghoang@users.noreply.github.com>
2025-04-03 09:41:00 +0700

    Update libc.c

--- libc.c
+++ libc.c
@@ -1,33 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <dlfcn.h>
-#include <stdint.h>
 
 int main() {
-    // Đường dẫn đến thư viện libc
-    const char *libc_path = "/lib/x86_64-linux-gnu/libc.so.6";
-
-    // Mở thư viện libc
-    void *libc_handle = dlopen(libc_path, RTLD_LAZY);
-    if (!libc_handle) {
-        fprintf(stderr, "dlopen failed: %s\n", dlerror());
-        return 1;
-    }
-
-    // Tìm địa chỉ của __libc_start_main
-    void (*__libc_start_main_ptr)(void);
-    __libc_start_main_ptr = (void (*)(void))dlsym(libc_handle, "__libc_start_main");
-    if (!__libc_start_main_ptr) {
-        fprintf(stderr, "dlsym failed: %s\n", dlerror());
-        dlclose(libc_handle);
-        return 1;
-    }
-
-    // In ra địa chỉ của __libc_start_main
-    printf("__libc_start_main address: %p\n", (void *)__libc_start_main_ptr);
-
-    // Đóng thư viện
-    dlclose(libc_handle);
-
+    printf("Hello, World!\n");
     return 0;
 }
